c4c64e3df1de0e0d48bc885308d105a6bc9c6f2e
Apoorva <aneeloji@unomaha.edu>
2017-01-18 23:17:16 -0600

    Added list

--- resources/views/Apoorva.php
+++ resources/views/Apoorva.php
@@ -7,8 +7,13 @@
 <body style="background-color:pink;">
 
 <h1>Trial 1</h1>
-<p>jsdoajda</p>
-
+<p>New List</p>
+<ul> 
+<li>
+Class Section </li>
+<li>
+Section 003</li>
+</ul>
 </body>
 </body>
 </html>
