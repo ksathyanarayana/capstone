a189d62f2c571629862c4dd3ad203d1efc2ca8a7
amathur24 <amathur@unomaha.edu>
2017-01-19 15:45:14 -0600

    testing branching

---
+++ resources/views/anusha.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>AN - Feature 6</h2>
+
+<body style="background-color:pink;">
+
+<h1>Testing</h1>
+
+<p>Still stuck</p>
+<p>New List</p>
+<ul> 
+<li>
+Class Section </li>
+<li>
+Section 003</li>
+</ul>
+</body>
+</body>
+</html>
